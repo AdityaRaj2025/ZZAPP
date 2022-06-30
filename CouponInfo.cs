2aef38b5b0b9389943d03a2dca0f5da586ae9311
Aditya Raj <38121656+AdityaRaj2025@users.noreply.github.com>
2022-06-30 14:10:58 +0900

    Add files via upload

---
+++ CouponInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZZAPP.Models
+{
+    public class CouponInfo
+    {       
+        public string CouponName  { get; set; }      
+        public int Quantity { get; set; }
+        public DateTime ExpirationDate { get; set; }    
+        public string Barcode { get; set; }
+        public bool FreeUse { get; set; }
+        public string Note { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ CouponViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms;
+using ZZAPP.Views;
+using ZZAPP.Models;
+using ZZAPP.Properties;
+
+namespace ZZAPP.ViewModels
+{
+    class CouponViewModel : BaseViewModel
+    {
+        private INavigation _Nav;
+
+        public CouponViewModel(INavigation nav)
+        {
+            _Nav = nav;
+
+            PageTitle = AppResources.CouponTitle;
+        }
+
+        private bool _Processing;
+        public bool IsProcessing
+        {
+            get
+            {
+                return _Processing;
+            }
+            set
+            {
+                SetProperty(ref _Processing, value);
+            }
+        }
+    }
+}
