7755e92844c1926cb7dfc318cc8b18817dbbbe11
Aditya Raj <38121656+AdityaRaj2025@users.noreply.github.com>
2022-07-19 17:11:30 +0900

    Update Splash.cs

--- Splash.cs
+++ Splash.cs
@@ -1,8 +1,25 @@
 namespace ZZAPP.Droid
 {
-    [Activity(Label = "ZZAPP", Icon = "@mipmap/icon", Theme = "@style/splashscreen", MainLauncher = true, LaunchMode = Android.Content.PM.LaunchMode.SingleTop, ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class SplashActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    [Activity(Label = "ZZAPP", Icon = "@mipmap/icon", Theme = "@style/splashscreen", MainLauncher = true, LaunchMode = Android.Content.PM.LaunchMode.SingleTask, ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class SplashActivity : Activity
     {
+    
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            var data = Intent.GetStringExtra(NotificationCenter.ReturnRequest);
+            var mainIntent = new Intent(Application.Context, typeof(MainActivity));
+
+            mainIntent.SetFlags(ActivityFlags.SingleTop);
+            if (!string.IsNullOrWhiteSpace(data))
+            {
+                mainIntent.PutExtra((NotificationCenter.ReturnRequest, data));
+            }
+
+            StartActivity(mainIntent);
+        }
+        
         protected override void OnResume()
         {
             base.OnResume();
