62623241c520bbf7a837c6cde010aec2da7ee3ed
Aditya Raj <38121656+AdityaRaj2025@users.noreply.github.com>
2022-11-29 17:39:09 +0900

    Update ReservationRoomTypePage.cs

--- ReservationRoomTypePage.cs
+++ ReservationRoomTypePage.cs
@@ -1,137 +1,97 @@
- // For editing the time portion of the start date and time
-private TimeSpan[] _StartTimeValues { get; set; }
-public string[] StartTimeNames { get; set; }
-private int _StartTimeIndex = -1;
-public int StartTimeIndex
+public ReservationRoomTypePage(ReservationViewModel model)
 {
-	get { return _StartTimeIndex; }
-	set
-	{
-		_StartTimeIndex = value;
+	InitializeComponent();
 
-		if (value >= 0)
-		{
-			Item.StartTime = Item.StartTime.Date.Add(_StartTimeValues[value]);
-			Item.EndTime = Item.StartTime.AddMinutes(Item.Duration);					  
-		}
-
-		FilterCourses();
-
-		OnPropertyChanged(nameof(StartTimeDisp));	
-	}
-}
-public string StartTimeDisp => StartTimeNames != null && StartTimeIndex >= 0 && StartTimeIndex < StartTimeNames.Length ? StartTimeNames[StartTimeIndex] : null;
-
- // For editing usage time
-private TimeSpan[] _DurationValues { get; set; }
-public string[] DurationNames { get; set; }
-private int _DurationIndex = -1;
-public int DurationIndex
-{
-	get { return _DurationIndex; }
-	set
-	{
-		_DurationIndex = value;
-
-		if (value >= 0)
-		{
-			Item.Duration = (int)_DurationValues[value].TotalMinutes;
-			Item.EndTime = Item.StartTime.AddMinutes(Item.Duration);
-		}
-
-		OnPropertyChanged(nameof(DurationDisp));
-	}   
-}
-public string DurationDisp => IsFreeTime ? 
-	DurationString(RoomCoursesSel[_RoomCourseIndex].TimeRange.GetEndDateTime(Item.StartTime) - Item.StartTime) :
-	DurationNames != null && DurationIndex >= 0 && DurationIndex < DurationNames.Length ? DurationNames[DurationIndex] : null;    
+	_Model = model;
+	BindingContext = _Model;
 	
-	
-// For RoomType
-public RoomType[] RoomTypesSel { get; set; }
-private int _RoomTypeIndex = -1;
-public int RoomTypeIndex
-{
-	get { return _RoomTypeIndex; }
-	set
+	Dictionary<int, List<KeyValuePair<string, string>>> keyValuePairs = _Model.keyValuePairs;        
+ 
+	_Model.IsProcessing = true;
+
+	//For getting times 
+	for (int i = 0; i < _Model.StartTimeNames.Length; i++)
 	{
-		_RoomTypeIndex = value;
-
-		if (value >= 0)
-		{
-			Item.RoomType = RoomTypesSel[value].Code;
-			Item.Status = RoomTypesSel[value].RStatus;                  
-		}
-		FilterCourses();
-
-		OnPropertyChanged(nameof(RoomTypeDisp));
+		string times = _Model.StartTimeNames[i];
+		timelist.ColumnDefinitions.Add(new ColumnDefinition());
+		Label time = new Label() { Text = times, WidthRequest = 80, VerticalTextAlignment = TextAlignment.Center, BackgroundColor = Color.Orange };              
+		timelist.Children.Add(time, i, 0);
 	}
-}
-public string RoomTypeDisp =>
-	Config != null && RoomTypeIndex >= 0 && RoomTypeIndex < RoomTypesSel.Length ? RoomTypesSel[RoomTypeIndex].Name : null;	
-	
+	status.WidthRequest = timelist.Width;
 
-// For RoomCourse
-public RoomCourse[] RoomCoursesSel { get; set; }
-private int _RoomCourseIndex = -1;
-public int RoomCourseIndex
-{
-	get { return _RoomCourseIndex; }
-	set
+	//For getting RoomTypes 
+	for (int i = 0; i < _Model.RoomTypesSel.Length; i++)
 	{
-		_RoomCourseIndex = value;
-
-		Item.RoomCourse = value >= 0 ? RoomCoursesSel[value].Code : 0;
-		OnPropertyChanged(nameof(RoomCourseDisp));
-
-		IsFreeTime = Item.RoomCourse > 0 && RoomCoursesSel[value].IsFreeTime;
-		FilterDurationValues();
-
-		OnPropertyChanged(nameof(DurationDisp));
-	}
-}
-public string RoomCourseDisp => 
-	Config != null && RoomCourseIndex >= 0 && RoomCourseIndex < RoomCoursesSel.Length ? RoomCoursesSel[RoomCourseIndex].Name : null;
-public bool IsFreeTime;
-
-	
-
-//Filter Courses
-private void FilterCourses()
-{
-	var current = RoomCoursesSel != null && RoomCourseIndex >= 0 && RoomCourseIndex < RoomCoursesSel.Length ? RoomCoursesSel[RoomCourseIndex] : null;
-	var newidx = -1;
-	var filtered = new List<RoomCourse>();
-
-	foreach (var rc in Config.RoomCourses)
-	{
-		// Is the course selectable for the room type being selected?
-		if (_RoomTypeIndex < 0 || !RoomTypesSel[_RoomTypeIndex].RoomCourses.Contains(rc.Code))
-		{
-			continue;
-		}
-
-		// Does the start time fall within the valid time?
-		if (_StartTimeIndex < 0 || !rc.TimeRange.Contains(_StartTimeValues[_StartTimeIndex]) || rc.TimeRange.EndTime == _StartTimeValues[_StartTimeIndex])
-		{
-			continue;
-		}
-
-		filtered.Add(rc);
-		if (rc == current)
-		{
-			newidx = filtered.Count - 1;
-		}
+		string roomtypes = _Model.RoomTypesSel[i].Name; 
+		
+		roomtype.RowDefinitions.Add(new RowDefinition() { Height = 50 });
+		Label room = new Label() { Text = roomtypes, WidthRequest = 200, BackgroundColor = Color.Orange, TextColor = Color.Black, VerticalTextAlignment = TextAlignment.Center };             
+		roomtype.Children.Add(room, 0, i);
 	}
 
-	// // Automatically select if only one course is available for selection
-	if (filtered.Count == 1)
-	{
-		newidx = 0;
+	//For getting RoomTypes Status
+	int index = 0;
+	foreach (var item in _Model.RoomTypesSel)
+	{                
+		status.RowDefinitions.Add(new RowDefinition() { Height = 50 });
+
+		List<KeyValuePair<string, string>> temp = keyValuePairs[item.Code];
+	   
+		for (int i = 0; i < _Model.StartTimeNames.Length; i++)
+		{
+			string filterStartTimeNames = _Model.StartTimeNames[i];
+			string statusFinal = "";
+		
+			foreach (KeyValuePair<string, string> timeValue in temp)
+			{                     
+				if (filterStartTimeNames == timeValue.Key)
+				{
+					statusFinal = timeValue.Value;
+					//Console.WriteLine("LoopCheck Nested Locked Value: " + timeValue.Key);
+				}
+				else
+				{
+					//Console.WriteLine("LoopCheck Nested Not Matched: " + timeValue.Key);
+				}
+			}
+
+			if (index < 1) { status.ColumnDefinitions.Add(new ColumnDefinition() { Width = 80 }); }
+
+			statuslabel = new Label() { Text = statusFinal, WidthRequest = 80, VerticalTextAlignment = TextAlignment.Center, HorizontalTextAlignment = TextAlignment.Center, BackgroundColor = Color.White, TextColor = Color.DarkRed };              
+			status.Children.Add(statuslabel, i, index);                  
+			statuslabel.GestureRecognizers.Add((new TapGestureRecognizer
+			{
+				Command = new Command(async (obj) =>
+				{
+					Label temp = obj as Label;
+					Grid gridtemp = temp.Parent as Grid;
+
+					int position = gridtemp.Children.IndexOf(temp);                           
+					int columnindex =  position % timelist.Children.Count;                           
+					int rowindex = (position / timelist.Children.Count);                           
+					string timevalue = ((Label)timelist.Children[columnindex]).Text;                          
+					roomtypealue = ((Label)roomtype.Children[rowindex]).Text;                            
+					int indexOfTimeValue = Array.IndexOf(_Model.StartTimeNames, timevalue);                                                     
+					int indexRoomType = Array.FindIndex(_Model.RoomTypesSel, t => t.Name == roomtypealue);
+																						
+											
+					if (statuslabel.Text == "TEL")
+					{
+						statuslabel.BackgroundColor = Color.GreenYellow;
+						await DisplayAlert("Info", "There are no vacancies", "OK");
+					}
+					else
+					{					   												
+						_Model.StartTimeIndex = indexOfTimeValue;
+						_Model.RoomTypeIndex = indexRoomType;
+						await Navigation.PopAsync();                                   
+					}			   
+				}
+				
+				}),
+				CommandParameter = statuslabel,
+			}));
+		}
+		index++;
 	}
-
-	RoomCoursesSel = filtered.ToArray();
-	OnPropertyChanged(nameof(RoomCoursesSel));
-
-	RoomCourseIndex = newidx;            
-}
+} 
